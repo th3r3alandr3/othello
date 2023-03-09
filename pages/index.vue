e2359b1677abeeea76902aeee9585edeba52259d
Andre <andre@eidemann.de>
2023-03-09 22:21:06 +0100

    Fix modal

--- pages/index.vue
+++ pages/index.vue
@@ -81,103 +81,103 @@ function update() {
         left: 0;
         height: 100%;
     }
+}
 
-    .wrapper {
-        position: relative;
-        width: 100%;
-        max-width: 42rem;
-        height: 100%;
+.wrapper {
+    position: relative;
+    width: 100%;
+    max-width: 42rem;
+    height: 100%;
 
-        @media (min-width: 768px) {
-            height: auto;
-        }
+    @media (min-width: 768px) {
+        height: auto;
+    }
+}
 
-        .content {
-            position: relative;
-            background-color: #fff;
-            border-radius: 0.5rem;
-            box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+.content {
+    position: relative;
+    background-color: #fff;
+    border-radius: 0.5rem;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+}
 
-            .header {
-                display: flex;
-                padding: 1rem;
-                justify-content: space-between;
-                align-items: flex-start;
-                border-top-left-radius: 0.25rem;
-                border-top-right-radius: 0.25rem;
-                border-bottom-width: 1px;
+.header {
+    display: flex;
+    padding: 1rem;
+    justify-content: space-between;
+    align-items: flex-start;
+    border-top-left-radius: 0.25rem;
+    border-top-right-radius: 0.25rem;
+    border-bottom-width: 1px;
+}
 
-                .title {
-                    color: #111827;
-                    font-size: 1.25rem;
-                    line-height: 1.75rem;
-                    font-weight: 600;
-                }
-            }
+.title {
+    color: #111827;
+    font-size: 1.25rem;
+    line-height: 1.75rem;
+    font-weight: 600;
+}
 
-            .body {
-                padding: 1.5rem;
-                margin-top: 1.5rem;
+.body {
+    padding: 1.5rem;
+    margin-top: 1.5rem;
+}
 
-                .text {
-                    color: #6b7280;
-                    font-size: 1rem;
-                    line-height: 1.5rem;
-                    line-height: 1.625;
-                }
-            }
+.text {
+    color: #6b7280;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    line-height: 1.625;
+}
 
-            .footer {
-                display: flex;
-                padding: 1.5rem;
-                margin-left: 0.5rem;
-                justify-content: flex-end;
-                align-items: center;
-                border-bottom-right-radius: 0.25rem;
-                border-bottom-left-radius: 0.25rem;
-                border-top-width: 1px;
-                border-color: #e5e7eb;
+.footer {
+    display: flex;
+    padding: 1.5rem;
+    margin-left: 0.5rem;
+    justify-content: flex-end;
+    align-items: center;
+    border-bottom-right-radius: 0.25rem;
+    border-bottom-left-radius: 0.25rem;
+    border-top-width: 1px;
+    border-color: #e5e7eb;
+}
 
-                .button-restart {
-                    padding-top: 0.625rem;
-                    padding-bottom: 0.625rem;
-                    padding-left: 1.25rem;
-                    padding-right: 1.25rem;
-                    background-color: #1d4ed8;
-                    color: #fff;
-                    font-size: 0.875rem;
-                    line-height: 1.25rem;
-                    font-weight: 500;
-                    text-align: center;
-                    border-radius: 0.5rem;
+.button-restart {
+    padding-top: 0.625rem;
+    padding-bottom: 0.625rem;
+    padding-left: 1.25rem;
+    padding-right: 1.25rem;
+    background-color: #1d4ed8;
+    color: #fff;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    font-weight: 500;
+    text-align: center;
+    border-radius: 0.5rem;
 
-                    :hover {
-                        background-color: #1e40af;
-                    }
+    :hover {
+        background-color: #1e40af;
+    }
 
-                }
+}
 
-                .button-cancel {
-                    padding-top: 0.625rem;
-                    padding-bottom: 0.625rem;
-                    padding-left: 1.25rem;
-                    padding-right: 1.25rem;
-                    background-color: #fff;
-                    color: #6b7280;
-                    font-size: 0.875rem;
-                    line-height: 1.25rem;
-                    font-weight: 500;
-                    border-radius: 0.5rem;
-                    border-width: 1px;
-                    border-color: #e5e7eb;
+.button-cancel {
+    padding-top: 0.625rem;
+    padding-bottom: 0.625rem;
+    padding-left: 1.25rem;
+    padding-right: 1.25rem;
+    background-color: #fff;
+    color: #6b7280;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    font-weight: 500;
+    border-radius: 0.5rem;
+    border-width: 1px;
+    border-color: #e5e7eb;
 
-                    :hover {
-                        background-color: #f3f4f6;
-                        color: #111827;
-                    }
-                }
-            }
-        }
+    :hover {
+        background-color: #f3f4f6;
+        color: #111827;
     }
 }
 </style>
